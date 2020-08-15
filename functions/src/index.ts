6bd290af17b94047e8f3420c4b6c661534395c0c
Zizhen Song <zizhensong@gmail.com>
2020-08-15 15:51:03 -0700

    implimented entity-extraction

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,27 +16,3 @@ app.use(authentication);
 
 
 exports.widgets = functions.https.onRequest(app);
-/**
- * TODO(developer): Uncomment the following line to run this code.
- */
-// const text = 'Your text to analyze, e.g. Hello, world!';
-
-// Prepares a document, representing the provided text
-const document = {
-  content: text,
-  type: 'PLAIN_TEXT',
-};
-
-// Detects entities in the document
-const [result] = await client.analyzeEntities({document});
-
-const entities = result.entities;
-
-console.log('Entities:');
-entities.forEach(entity => {
-  console.log(entity.name);
-  console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
-  if (entity.metadata && entity.metadata.wikipedia_url) {
-    console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}`);
-  }
-});
