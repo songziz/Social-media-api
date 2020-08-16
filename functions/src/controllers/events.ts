6d20cdae11d7cd7a7c82635a876725a60db897bd
Zizhen Song <zizhensong@gmail.com>
2020-08-16 10:01:46 -0700

    getEvent() imp.

--- functions/src/controllers/events.ts
+++ functions/src/controllers/events.ts
@@ -10,7 +10,7 @@ export const createEvent = async (req: express.Request, res: express.Response) =
   }
 
   const {event} = req.body;
-  
+
   const tags = await extractEntity(event.description);
   const imageUrl = getImage(tags);
 
@@ -18,7 +18,7 @@ export const createEvent = async (req: express.Request, res: express.Response) =
   for (const tag of tags) {
     newTags[tag[0]] = tag[1];
   }
-  
+
   const data = {
     createdBy: event.username,
     description: event.description,
@@ -32,19 +32,19 @@ export const createEvent = async (req: express.Request, res: express.Response) =
   try {
     await admin.firestore().runTransaction(async () => {
       const userDoc = admin.firestore().collection('users').doc(event.createdBy);
-  
+
       const userDataTags = await userDoc.get().then(doc => doc.data()!.tags);
-  
+
       for (const key of Object.keys(newTags)) {
         if (userDataTags[key] === undefined) {
           userDataTags[key] = 0;
         }
-  
+
         userDataTags[key] = userDataTags[key] + newTags[key];
       }
-      
+
       const eventId = await admin.firestore().collection('events').add(data).then((doc) => doc.id);
-  
+
       const summaryData = {
         createdBy: data.createdBy,
         current: false,
@@ -53,7 +53,7 @@ export const createEvent = async (req: express.Request, res: express.Response) =
         uid: eventId,
         username: event.username,
       }
-  
+
       await userDoc.collection('events').doc(eventId).set(summaryData);
     });
     res.sendStatus(201);
@@ -64,3 +64,31 @@ export const createEvent = async (req: express.Request, res: express.Response) =
 
 
 // getEvent
+
+export const getEvent = (req: express.Request, res: express.Response) => {
+  if (!req.params.uid) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const uid : string = req.params.uid;
+
+  admin.firestore().collection('events').doc(uid).get().then(function(doc) {
+    if (doc.exists) {
+      const data = doc.data();
+      const result = {
+        createdBy: data!.createdBy,
+        description: data!.description,
+        image: data!.image,
+        name:data!.name,
+        postedOn:data!.postedOn,
+        slots:data!.slots,
+        username: data!.username,
+        uid: uid,
+      };
+      res.status(200).json(result);
+    } else {
+      res.sendStatus(404);
+    }
+  }).catch((error) => res.status(500).send(error.message));
+};
