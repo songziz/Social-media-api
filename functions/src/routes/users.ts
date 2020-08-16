c55f50c781fabbf2bb785a59964091fe433d163b
Samuel Berensohn <samberensohn@gmail.com>
2020-08-15 22:14:34 -0700

    Assigned Routes

--- functions/src/routes/users.ts
+++ functions/src/routes/users.ts
@@ -2,21 +2,27 @@ import * as express from 'express';
 import {
   getUser,
   createUser,
-  updateUser,
+  updateTags,
   deleteUser,
-  getUserEvents,
+  joinEvent,
+  sendFriendRequest,
+  acceptFriendRequest,
 } from '../controllers/users';
 
 const router : express.Router = express.Router();
 
 router.get('/:uid', getUser);
 
-router.get('/:uid/events', getUserEvents);
-
 router.post('/', createUser);
 
-router.patch('/:uid', updateUser);
+router.patch('/:uid', updateTags);
 
 router.delete('/:uid', deleteUser);
+
+router.post('/:uid/requests/send', sendFriendRequest);
+
+router.post('/:uid/requests/accept', acceptFriendRequest);
+
+router.post('/:uid/events/', joinEvent);
 
 export default router;
